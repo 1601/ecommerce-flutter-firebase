6acedb151b51f3eb0bd8271efac7799df3eb2138
Gelo Ilagan <gelo@ussi.com.ph>
2020-07-18 19:19:26 +0800

    remove test functions

--- lib/app/home/map/map_page.dart
+++ lib/app/home/map/map_page.dart
@@ -66,12 +66,6 @@ class MapState extends State<MapScaffold> {
     zoom: 12,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -102,11 +96,6 @@ class MapState extends State<MapScaffold> {
           myLocationEnabled: true,
           padding: EdgeInsets.only(bottom: 175.0, right: 23.0)),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
 
