7502d0953d685617b5fc192630fa79c2ff02a412
Gelo Ilagan <gelo@ussi.com.ph>
2020-07-18 11:23:51 +0800

    implemented sqflite for cart #3

--- lib/app/home/models/cart.dart
+++ lib/app/home/models/cart.dart
@@ -1,10 +1,16 @@
+import 'package:ecommerce_flutter_firebase/database/database.dart';
 import 'package:flutter/foundation.dart';
 import 'package:ecommerce_flutter_firebase/app/home/models/menu.dart';
 
 class CartModel extends ChangeNotifier {
   MenuModel _menu;
 
-  final List<int> _itemIds = [];
+  List<int> _itemIds = [];
+  final db = CartDatabase();
+
+  CartModel() {
+    setupList();
+  }
 
   MenuModel get menu => _menu;
 
@@ -23,16 +29,25 @@ class CartModel extends ChangeNotifier {
 
   void add(Item item) {
     _itemIds.add(item.hashCode);
+    db.addToCart(item.hashCode);
     notifyListeners();
   }
 
   void remove(Item item) {
     _itemIds.remove(item.hashCode);
+    db.removeFromCart(item.hashCode);
     notifyListeners();
   }
 
   void empty() {
     _itemIds.clear();
+    db.emptyCart();
+    notifyListeners();
+  }
+
+  void setupList() async {
+    var itemIds = await db.fetchAll();
+    this._itemIds = itemIds;
     notifyListeners();
   }
 }
---
+++ lib/database/database.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class CartDatabase {
+  static final CartDatabase _instance = CartDatabase._();
+  static Database _database;
+
+  CartDatabase._();
+
+  factory CartDatabase() {
+    return _instance;
+  }
+
+  Future<Database> get db async {
+    if (_database != null) {
+      return _database;
+    }
+
+    _database = await init();
+
+    return _database;
+  }
+
+  Future<Database> init() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String dbPath = join(directory.path, 'database1.db');
+    var database = openDatabase(dbPath,
+        version: 1, onCreate: _onCreate, onUpgrade: _onUpgrade);
+
+    return database;
+  }
+
+  void _onCreate(Database db, int version) {
+    db.execute('''
+      CREATE TABLE cart(
+        itemId INTEGER)
+    ''');
+    print("Database was created!");
+  }
+
+  void _onUpgrade(Database db, int oldVersion, int newVersion) {
+    // Run migration according database versions
+  }
+
+  Future<int> addToCart(int itemId) async {
+    var client = await db;
+    return client.rawInsert('INSERT INTO cart(itemID) VALUES ($itemId)');
+  }
+
+  Future<List<int>> fetchAll() async {
+    var client = await db;
+    var res = await client.query('cart');
+    List<int> itemIds = [];
+    if (res.isNotEmpty) {
+      res.forEach((element) {
+        itemIds.add(element['itemId']);
+      });
+      print(itemIds);
+      return itemIds;
+    }
+    return itemIds;
+  }
+
+  Future<void> removeFromCart(int itemId) async {
+    var client = await db;
+    return client.delete('cart', where: 'itemId = ?', whereArgs: [itemId]);
+  }
+
+  Future<void> emptyCart() async {
+    var client = await db;
+    return client.delete('cart');
+  }
+
+  Future closeDb() async {
+    var client = await db;
+    client.close();
+  }
+}
